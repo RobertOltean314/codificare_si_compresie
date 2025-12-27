da2a5651defc3fa8c3d9b9ba0744cc0a4b4f4e8d
RobertOLtean314 <olteanrobertadrian314@gmail.com>
2025-12-27 20:18:03 +0200

    separated business logic into its separate ImageManager class. Implemented creation for error_matrix

--- predictiv/src/main.rs
+++ predictiv/src/main.rs
@@ -1,53 +1,99 @@
-use std::path::Path;
+use core::error;
+use std::path::{self, Path};
 
-pub fn read_image(path: &Path) -> Result<(usize, usize, Vec<Vec<u8>>), bmp::BmpError> {
-    let img = bmp::open(path)?;
-    let width = img.get_width() as usize;
-    let height = img.get_height() as usize;
-    let mut original_image: Vec<Vec<u8>> = Vec::with_capacity(height);
-    for y in 0..height {
-        let mut row = Vec::with_capacity(width);
-        for x in 0..width {
-            let pixel = img.get_pixel(x as u32, y as u32);
-            let gray = pixel.r;
-            row.push(gray);
-        }
-        original_image.push(row);
-    }
-    Ok((width, height, original_image))
+#[derive(Debug)]
+pub struct MyCustomImage {
+    height: usize,
+    width: usize,
+    data: Vec<Vec<i32>>,
 }
 
+pub struct ImageManager;
+
+impl ImageManager {
+    pub fn read_image(&self, path: &Path) -> Result<MyCustomImage, bmp::BmpError> {
+        let img = bmp::open(path)?;
+        let width = img.get_width() as usize;
+        let height = img.get_height() as usize;
+
+        let mut data = Vec::with_capacity(height);
+
+        for y in 0..height {
+            let mut row = Vec::with_capacity(width);
+            for x in 0..width {
+                let pixel = img.get_pixel(x as u32, y as u32);
+                row.push(pixel.r as i32);
+            }
+            data.push(row);
+        }
+
+        Ok(MyCustomImage {
+            width,
+            height,
+            data,
+        })
+    }
+
+    fn get_prediction_matrix(&self, original_img: &MyCustomImage) -> Vec<Vec<i32>> {
+        let mut data = vec![vec![0i32; original_img.width]; original_img.height];
+        data[0][0] = 128;
+
+        for j in 1..original_img.height {
+            data[j][0] = original_img.data[j - 1][0];
+        }
+
+        for i in 1..original_img.height {
+            data[0][i] = original_img.data[0][i - 1];
+        }
+
+        for i in 1..original_img.height {
+            for j in 1..original_img.width {
+                let a = original_img.data[i][j - 1]; // left
+                let b = original_img.data[i - 1][j]; // above
+                let c = original_img.data[i - 1][j - 1]; // diagonal
+                data[i][j] = a + b - c;
+            }
+        }
+        data
+    }
+
+    pub fn predict(&self, path: &Path) -> Result<MyCustomImage, bmp::BmpError> {
+        let original_img = self.read_image(path)?;
+        let predicted = self.get_prediction_matrix(&original_img);
+
+        let height = original_img.height;
+        let width = original_img.width;
+        let original_data = &original_img.data;
+
+        let mut error_data = vec![vec![0i32; width]; height];
+
+        for y in 0..height {
+            for x in 0..width {
+                error_data[y][x] = original_data[y][x] - predicted[y][x];
+            }
+        }
+
+        Ok(MyCustomImage {
+            height,
+            width,
+            data: error_data,
+        })
+    }
+}
 fn main() -> Result<(), bmp::BmpError> {
     let path = Path::new("../Lenna256an.bmp");
-    let (width, height, original_image) = read_image(path)?;
+    let manager = ImageManager;
 
-    let mut predicted_image = vec![vec![0i32; width]; height];
+    let error_img = manager.predict(path)?;
 
-    predicted_image[0][0] = 128;
+    println!("Error image size: {}×{}", error_img.width, error_img.height);
 
-    for j in 1..width {
-        predicted_image[0][j] = original_image[0][j - 1] as i32;
-    }
-
-    for i in 1..height {
-        predicted_image[i][0] = original_image[i - 1][0] as i32;
-    }
-
-    for i in 1..height {
-        for j in 1..width {
-            let a = original_image[i][j - 1] as i32;
-            let b = original_image[i - 1][j] as i32;
-            let c = original_image[i - 1][j - 1] as i32;
-            predicted_image[i][j] = a + b - c;
+    for i in 0..error_img.height {
+        for j in 0..error_img.width {
+            print!("{:6}", error_img.data[i][j]);
         }
-    }
-
-    for row in &predicted_image {
-        for &pixel in row {
-            print!("{:4}", pixel);
-        }
-        println!();
     }
 
     Ok(())
 }
+
